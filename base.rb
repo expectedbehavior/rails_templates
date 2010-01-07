8eb3bceac10c4da6feaed43ed534bb854b469536
Joel Meador <joel@expectedbehavior.com>
2010-01-07 00:44:34 -0500

    ask to use sudo. this is important if you are using the ruby_switcher

--- base.rb
+++ base.rb
@@ -99,8 +99,9 @@ gem 'cucumber-rails', :env => "test"
 # gem 'sqlite3-ruby', :lib => 'sqlite3'
 # gem 'hpricot', :source => 'http://code.whytheluckystiff.net'
 # gem 'RedCloth', :lib => 'redcloth'
-rake('gems:install', :sudo => true)
-rake('gems:install', :env => "test", :sudo => true)
+install_gems_using_sudo = /[Yy]/ =~ HighLine.ask("install gems using sudo?[Yn]") {|q| q.default = 'Y'; q.validate = /[YyNn]/}
+rake('gems:install', :sudo => install_gems_using_sudo)
+rake('gems:install', :env => "test", :sudo => install_gems_using_sudo)
 git :add => '.'
 git :commit => "-m 'gems'"
 
