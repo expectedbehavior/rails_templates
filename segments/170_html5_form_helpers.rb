dcfcaf57e11e1ab50705602432d510e6e2f59222
Joel Meador <joel@expectedbehavior.com>
2010-12-17 02:22:45 -0500

    html5 form helpers

---
+++ segments/170_html5_form_helpers.rb
@@ -0,0 +1,32 @@
+# https://github.com/jessedearing/html5_rails2_plugin.git
+# good for doing special types of input fields; mostly useful for mobile dev e.g. phone_field
+# <%= f.search_field :search, "Search something..." %>
+# <%= f.telephone_field :phone, "123-456-7890" %>
+# <%= f.number_field :number, 5, :step => 1, :min => 0, :max => 10 %>
+# <%= f.range_field :range, 5, :min => 0, :max => 10 %>
+# <%= f.email_field :email, "person@email.com" %>
+# <%= f.url_field :url, "http://github.com" %>
+# <%= f.datetime_field :publish_at, 5.hours.from_now %>
+# <%= f.color_field :background_color, "#f34be3" %>
+
+class Html5FormHelpers < TemplateSegment
+  def required?
+    true
+  end
+  
+  def starting_message
+    "Installing HTML5 Form Helpers"
+  end
+  
+  def ending_message
+    "now with more form input helpers"
+  end
+  
+  def commit_message
+    "html5 form helpers added"
+  end
+  
+  def run_segment
+    self.plugin 'fix_you_some_address_bar', :git => 'https://github.com/jessedearing/html5_rails2_plugin.git'
+  end
+end
